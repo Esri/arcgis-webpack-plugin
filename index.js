ec06b1f166a26cfb8cf0a907ea2d6a356d487eaa
rene rubalcava <rrubalcava@esri.com>
2018-04-17 09:43:55 -0700

    update moment replacement and files that are copied

--- index.js
+++ index.js
@@ -46,8 +46,13 @@ const requiredPlugins = [
     },
     {
       context: "node_modules",
-      from: "arcgis-js-api/themes/base/images/basemap-toggle-64.svg",
-      to: "arcgis-js-api/themes/base/images/basemap-toggle-64.svg"
+      from: "arcgis-js-api/themes/base/images/",
+      to: "arcgis-js-api/themes/base/images/"
+    },
+    {
+      context: "node_modules",
+      from: "arcgis-js-api/images/support/",
+      to: "arcgis-js-api/images/support/"
     },
     {
       context: "node_modules",
@@ -67,7 +72,7 @@ const requiredPlugins = [
     /^dojox\/gfx\/renderer!/,
     "dojox/gfx/svg"
   ),
-  new webpack.NormalModuleReplacementPlugin(/\/moment!/, "esri/plugins/moment")
+  new webpack.NormalModuleReplacementPlugin(/\/moment!/, "moment/moment")
 ];
 
 module.exports = class ArcGISPlugin {
